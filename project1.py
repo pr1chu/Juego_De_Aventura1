0a82a7a1fd66f45190c87cfee8fb9e74d6648944
Priscila <priscilaotaro.g26@gmail.com>
2026-07-16 11:59:12 -0300

    Agregando primeros condicionales

--- project1.py
+++ project1.py
@@ -1,5 +1,9 @@
 name = input("Hey, ¿cómo te llamas?: ")
 print("Hola, " + name + "! Bienvenidx a mi juego!")
 
-should_we_play = input("¿Quieres jugar? (sí/no): ")
+should_we_play = input("¿Quieres jugar? (sí/no): ").lower()
 
+if (should_we_play == "sí" or should_we_play == "si" or should_we_play == "yes"):
+    print("¡Genial! Vamos a jugar.")
+else:
+    print("OK, quizás en otra ocasión...")
